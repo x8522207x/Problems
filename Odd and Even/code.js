32bdeeaba74051edb2f6b3e5d0abf8fb799cdeaa
x8522207x <x8522207x@gmail.com>
2022-04-01 17:01:47 +0800

    upload files

---
+++ Odd and Even/code.js
@@ -0,0 +1,11 @@
+function calculate(bitString) {
+  let bitArray = bitString.split("");
+  let digit = bitArray.length - bitArray.findIndex(n => n === '1');
+  let remainOneCounts = bitArray.filter(n => n === "1").length - 1;
+  let actionSums = digit + remainOneCounts;
+  return actionSums;
+}
+
+let S='10011';
+
+document.write(calculate(S));
